5b199f6bce52c0e70346433af545ac0f2896afa7
Vinesh Kannan <vinesh.kannan@ambiencehealthcare.com>
2024-10-19 15:10:57 -0700

    Support URL direction on GitHub Pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/login';
-import Home from './pages/home';
+import { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
+import Login from "./pages/login";
+import Home from "./pages/home";
+
+const BASENAME = "myteam";
+const REDIRECT_PATHNAME_KEY = `myteam__pathname`;
+
+function AppRoutes() {
+  // Handle redirect on static site
+  const navigateTo = useNavigate();
+  useEffect(() => {
+    const redirectFullPathname = localStorage.getItem(REDIRECT_PATHNAME_KEY);
+    if (redirectFullPathname) {
+      localStorage.setItem(REDIRECT_PATHNAME_KEY, "");
+      const redirectPathname = redirectFullPathname.substring(BASENAME.length);
+      navigateTo(redirectPathname);
+    }
+  }, [navigateTo]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
 
 export default function App() {
   return (
-    <Router basename="/myteam/">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+    <Router basename={BASENAME}>
+      <AppRoutes />
     </Router>
   );
 }
